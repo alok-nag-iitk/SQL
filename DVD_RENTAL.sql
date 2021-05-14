f0f6156bbc1dafb3069c40c76cbe29e0c263cf90
Alok Nag <83603251+alok-nag-iitk@users.noreply.github.com>
2021-05-15 00:34:42 +0530

    Update DVD_RENTAL.sql

--- DVD_RENTAL.sql
+++ DVD_RENTAL.sql
@@ -1,3 +1,5 @@
+#1 (kata 6)
+
 SELECT 
   customer.customer_id as customer_id,
   customer.email as email,
@@ -13,3 +15,40 @@ ORDER BY 4 DESC
 LIMIT 10
 
 
+
+
+#2 (kata 7)
+
+SELECT 
+  id,
+  ASCII(name) as name,
+  birthday,
+  ASCII(race) as race
+  
+FROM demographics
+
+
+
+#3 (kata 7)
+
+SELECT
+  CASE
+    WHEN SUM(number1)%2 = 1 THEN MIN(number1)
+    ELSE MAX(number1)
+  END AS number1,
+  
+  CASE
+    WHEN SUM(number2)%2 = 1 THEN MIN(number1)
+    ELSE MAX(number2)
+  END AS number2
+  
+FROM numbers
+
+
+
+
+
+#4 (kata 6)
+
+
+  
