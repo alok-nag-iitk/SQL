35c2dbe1e25796fca2aefc94c7479d0176a40828
Alok Nag <83603251+alok-nag-iitk@users.noreply.github.com>
2021-05-29 17:28:47 +0530

    Update Salaries_Differences.sql

--- Salaries_Differences.sql
+++ Salaries_Differences.sql
@@ -1,3 +1,5 @@
+/*
+
 **Salaries Differences**
 
 Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments. Output just the difference in salaries.
