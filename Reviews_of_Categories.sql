e3cbc90c06265c102727b731a7cb08ee629ae054
Alok Nag <83603251+alok-nag-iitk@users.noreply.github.com>
2021-06-11 14:44:38 +0530

    Add files via upload

---
+++ Reviews_of_Categories.sql
@@ -0,0 +1,39 @@
+/*
+
+Reviews of Categories
+------------------------------------------
+
+Find the top business categories based on the total number of reviews. Output the category along with the total number of reviews. Order by total reviews in descending order.
+
+
+yelp_business
+----------------------
+business_id	varchar
+name		varchar
+neighborhood	varchar
+address		varchar
+city		varchar
+state		varchar
+postal_code	varchar
+latitude	float
+longitude	float
+stars		float
+review_count	int
+is_open		int
+categories	varchar
+
+*/
+
+SELECT
+    unnest(string_to_array(categories, ';')) AS categories, 
+    SUM(review_count) AS total_reviews
+FROM
+    yelp_business
+GROUP BY
+    1
+ORDER BY
+    total_reviews DESC;
+    
+
+
+
