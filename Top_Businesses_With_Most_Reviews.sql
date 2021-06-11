bbd25b1c8450b04fb6a22a2fad40c654b1b061b8
Alok Nag <83603251+alok-nag-iitk@users.noreply.github.com>
2021-06-11 14:51:51 +0530

    Add files via upload

---
+++ Top_Businesses_With_Most_Reviews.sql
@@ -0,0 +1,50 @@
+/*
+
+Top Businesses With Most Reviews
+-----------------------------------------
+Find the top 5 businesses with most reviews. Assume that each row has a unique business_id
+such that the total reviews for each business is listed on each row. Output the business name 
+along with the total number of reviews and order your results by the total reviews in descending order.
+
+
+yelp_business
+-------------------------
+business_id	varchar
+name		varchar
+neighborhood	varchar
+address		varchar
+city		varchar
+state		varchar
+postal_code	varchar
+latitude	float
+longitude	float
+stars		float
+review_count	int
+is_open		int
+categories	varchar
+
+*/
+
+SELECT
+    name, review_count
+FROM
+    yelp_business
+ORDER BY
+    review_count DESC
+LIMIT 5;
+
+
+/*
+
+Output
+
+name				review_count
+--------------------------------------------------
+Iron Chef			331
+Jacs Dining and Tap House	197
+Grimaldi's Pizzeria		187
+Signs Restaurant		120
+Kassab's			101
+
+*/
+
