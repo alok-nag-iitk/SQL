30b84eba3bb388cb46d034ef7fbce5e96293e4c1
Alok Nag <83603251+alok-nag-iitk@users.noreply.github.com>
2021-06-11 14:52:57 +0530

    Update distance_travelled.sql

--- distance_travelled.sql
+++ distance_travelled.sql
@@ -24,9 +24,14 @@ SELECT
     lyft_users.name as name,
     SUM(lyft_rides_log.distance) as total_distance
     
-FROM lyft_users
-INNER JOIN lyft_rides_log
-ON lyft_users.id = lyft_rides_log.user_id
-GROUP BY name
-ORDER BY total_distance DESC
+FROM 
+    lyft_users
+INNER JOIN 
+    lyft_rides_log
+ON 
+    lyft_users.id = lyft_rides_log.user_id
+GROUP BY
+    name
+ORDER BY 
+    total_distance DESC
 LIMIT 10;
